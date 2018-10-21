aeb5c635ca545cc67dba9835312c59da59114eb9
Chris <chrisfontas724@gmail.com>
2018-10-21 01:58:21 -0700

    can modify compression

--- fetch_files.py
+++ fetch_files.py
@@ -7,7 +7,6 @@ import random
 import shutil
 from CGvsPhoto import image_loader as il
 
-
 def load_images_from_dir(dir_name, shuffle = False) :
     
     # file extension accepted as image data
@@ -30,46 +29,72 @@ def load_images_from_dir(dir_name, shuffle = False) :
         return file_list
 
 
-filenames = ['data/train_file.csv', 'data/test_file.csv', 'data/validation_file.csv']
-outputdirs = ['train/', 'test/', 'validation/']
-
-index = 0
-for filename in filenames:
-
-    # make a directory
-    directory = filename.split('_')[0]
-    os.system('mkdir {}'.format(directory))
-
-    # copy over the files
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count > 15: # Change as necessary
-                break
-            file = row[0].split('.')[0] # get rid of .jpg
-            cmd = ' wget -N -c http://193.205.194.113/RAISE/NEF/{}.NEF -P {}'.format(file, directory)
-            print cmd
-            os.system(cmd)
-            line_count = line_count + 1
+def makeDatabaseDirectories():
+    # Outer database
+    os.system('mkdir {}'.format('Database'))
+    
+    # Train test and validation
+    os.system('mkdir {}'.format('Database/train'))
+    os.system('mkdir {}'.format('Database/test'))
+    os.system('mkdir {}'.format('Database/validation'))
+    
+    # Real photos
+    os.system('mkdir {}'.format('Database/train/Real'))
+    os.system('mkdir {}'.format('Database/test/Real'))
+    os.system('mkdir {}'.format('Database/validation/Real'))
+    
+    # CG Photos
+    os.system('mkdir {}'.format('Database/train/CGG'))
+    os.system('mkdir {}'.format('Database/test/CGG'))
+    os.system('mkdir {}'.format('Database/validation/CGG'))
 
 
-    # Convert .NEF files into 8-bit RGB images and save in "results" dir
-    cmd = 'NEFConverter/NEFConverter/bin/NEFConverter' + ' ' + directory + '/'
-    os.system(cmd)
+def main():
+    filenames = ['data/train_file.csv', 'data/test_file.csv', 'data/validation_file.csv']
+    outputdirs = ['train/', 'test/', 'validation/']
+    makeDatabaseDirectories()
 
-    # Load up all the converted images
-    result_dir = directory + '/results/'
-    final_images = load_images_from_dir(directory + '/results', shuffle = True)
-    numImages = len(final_images)
+    index = 0
+    for filename in filenames:
 
-    # Compress images and save in final database location
-    for i in range(numImages):
-        outfile = 'Database/' + outputdirs[index] + 'Real/' + final_images[i]
-        print outfile
-        image = Image.open(result_dir + final_images[i])
-        image.save(outfile, "JPEG", quality=75)
+        # make a directory
+        directory = filename.split('_')[0]
+        os.system('mkdir {}'.format(directory))
 
-    index = index+1
+        # copy over the files
+        with open(filename) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count > 15: # Change as necessary
+                    break
+                file = row[0].split('.')[0] # get rid of .jpg
+                cmd = ' wget -N -c http://193.205.194.113/RAISE/NEF/{}.NEF -P {}'.format(file, directory)
+                print cmd
+                os.system(cmd)
+                line_count = line_count + 1
+
+
+        # Convert .NEF files into 8-bit RGB images and save in "results" dir
+        # Using the LibRaw API to extract the iamge data and the SFML framework
+        # to save out the image as a PNG
+        cmd = 'NEFConverter/NEFConverter/bin/NEFConverter' + ' ' + directory + '/'
+        os.system(cmd)
+
+        # Load up all the converted images
+        result_dir = directory + '/results/'
+        final_images = load_images_from_dir(directory + '/results', shuffle = True)
+        numImages = len(final_images)
+
+        # Compress images and save in final database location
+        for i in range(numImages):
+            outfile = 'Database/' + outputdirs[index] + 'Real/' + final_images[i]
+            print outfile
+            image = Image.open(result_dir + final_images[i])
+            image.save(outfile, "JPEG", quality=15)
+
+        index = index+1
 
    
+if __name__== "__main__":
+    main()
