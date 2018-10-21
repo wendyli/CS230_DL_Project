e7744abb54d19e385224506232b910c51d5edd00
Chris <chrisfontas724@gmail.com>
2018-10-21 02:07:27 -0700

    take in jpeg compression as input

--- fetch_files.py
+++ fetch_files.py
@@ -1,33 +1,13 @@
 import csv
 import os
+import sys
 from PIL import ImageFile, Image
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import os, os.path
 import random
 import shutil
 from CGvsPhoto import image_loader as il
-
-def load_images_from_dir(dir_name, shuffle = False) :
-    
-    # file extension accepted as image data
-    valid_image_extension = [".jpg", ".jpeg",".gif",".png",".tga",".tif", ".JPG"]
-    
-    file_list = []
-    nb_image = 0
-    for filename in os.listdir(dir_name):
-        # check if the file is an image
-        extension = os.path.splitext(filename)[1]
-        if extension.lower() in valid_image_extension:
-            file_list.append(filename)
-            nb_image += 1
-
-    print('    ',nb_image,'images loaded')
-
-    if shuffle:
-        random.seed(42)
-        random.shuffle(file_list)
-        return file_list
-
+from CGvsPhoto.construct_DB import load_images_from_dir
 
 def makeDatabaseDirectories():
     # Outer database
@@ -53,6 +33,12 @@ def main():
     filenames = ['data/train_file.csv', 'data/test_file.csv', 'data/validation_file.csv']
     outputdirs = ['train/', 'test/', 'validation/']
     makeDatabaseDirectories()
+    
+    jpegCompression = 100
+    if len(sys.argv) > 1:
+        jpegCompression = int(sys.argv[1])
+    
+    print 'JPEG Compression: ', jpegCompression
 
     index = 0
     for filename in filenames:
@@ -61,25 +47,25 @@ def main():
         directory = filename.split('_')[0]
         os.system('mkdir {}'.format(directory))
 
-        # copy over the files
-        with open(filename) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
-            for row in csv_reader:
-                if line_count > 15: # Change as necessary
-                    break
-                file = row[0].split('.')[0] # get rid of .jpg
-                cmd = ' wget -N -c http://193.205.194.113/RAISE/NEF/{}.NEF -P {}'.format(file, directory)
-                print cmd
-                os.system(cmd)
-                line_count = line_count + 1
-
-
-        # Convert .NEF files into 8-bit RGB images and save in "results" dir
-        # Using the LibRaw API to extract the iamge data and the SFML framework
-        # to save out the image as a PNG
-        cmd = 'NEFConverter/NEFConverter/bin/NEFConverter' + ' ' + directory + '/'
-        os.system(cmd)
+#        # copy over the files
+#        with open(filename) as csv_file:
+#            csv_reader = csv.reader(csv_file, delimiter=',')
+#            line_count = 0
+#            for row in csv_reader:
+#                if line_count > 15: # Change as necessary
+#                    break
+#                file = row[0].split('.')[0] # get rid of .jpg
+#                cmd = ' wget -N -c http://193.205.194.113/RAISE/NEF/{}.NEF -P {}'.format(file, directory)
+#                print cmd
+#                os.system(cmd)
+#                line_count = line_count + 1
+#
+#
+#        # Convert .NEF files into 8-bit RGB images and save in "results" dir
+#        # Using the LibRaw API to extract the iamge data and the SFML framework
+#        # to save out the image as a PNG
+#        cmd = 'NEFConverter/NEFConverter/bin/NEFConverter' + ' ' + directory + '/'
+#        os.system(cmd)
 
         # Load up all the converted images
         result_dir = directory + '/results/'
@@ -91,7 +77,7 @@ def main():
             outfile = 'Database/' + outputdirs[index] + 'Real/' + final_images[i]
             print outfile
             image = Image.open(result_dir + final_images[i])
-            image.save(outfile, "JPEG", quality=15)
+            image.save(outfile, "JPEG", quality=jpegCompression)
 
         index = index+1
 
