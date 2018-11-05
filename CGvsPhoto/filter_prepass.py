846aaf694cea4569694ec582276ef262511cc393
Chris <chrisfontas724@gmail.com>
2018-11-04 16:23:55 -0800

    update filters

--- CGvsPhoto/filter_prepass.py
+++ CGvsPhoto/filter_prepass.py
@@ -3,19 +3,55 @@
 import tensorflow as tf
 from PIL import Image
 
-
-# Thre high pass filters as described in the paper
-square5x5 = [[-1,2,-2,2,-1], [2,-6,8,-6,2], [-2,8,-12,8,-2], [2,-6,8,-6,2],[-1,2,-2,2,-1]]
-edge3x3 = [[0,0,0,0,0], [0,0,-1,0,0], [0,-1,4,-1,0], [0,0,-1,0,0], [0,0,0,0,0]]
-square3x3 = [[0,0,0,0,0], [0,-1,2,-1,0], [0,2,-4,2,0], [0,-1,2,-1,0], [0,0,0,0,0]]
-
-
 def grayscale():
     pass
 
 def highpassFilter(batch):
-    filters = tf.constant([0, 1, 0, 1, 2, 1, 1, 0, 3, 1, 1, 0, 4, 4, 5, 4], tf.float32)
-    temp2 = tf.reshape(temp, [2, 2, 2, 2])
+
+    # For now, make session
+    sess = tf.Session()
     
-    pass
+    # Thre high pass filters as described in the paper
+    filters = tf.constant(# Filter 1 - Shape 5x5
+                          [-1,2,-2,2,-1,
+                          2,-6,8,-6,2,
+                          -2,8,-12,8,-2,
+                          2,-6,8,-6,2,
+                          -1,2,-2,2,-1,
+                            
+                          # Filter 2  - Edge 3x3 with padding
+                          0,0,0,0,0,
+                          0,0,-1,0,0,
+                          0,-1,4,-1,0,
+                          0,0,-1,0,0,
+                          0,0,0,0,0,
+                            
+                          # Filter 3  - Shape 3x3 with padding
+                          0,0,0,0,0,
+                          0,-1,2,-1,0,
+                          0,2,-4,2,0,
+                          0,-1,2,-1,0,
+                          0,0,0,0,0],
+                          
+                          # 3 5x5 filters
+                          shape = [5,5,1,3],
+                          
+                          # Name
+                          name = 'HighpassFilters'
+                        )
+                        
+                        
+                        
+    with sess.as_default():
+        print(filters.eval())
+
+
+
+# For Testing purposes
+def main():
+    print("Running High Pass Filter Test!")
+    highpassFilter(None)
+
+if __name__== "__main__":
+    main()
 
